7cc4a744a4c1ebd1b623e02854e87c579ebbff68
loydfordfarrow <loydfordfarrow@gmail.com>
2026-03-18 05:47:07 +0000

    20260318-13:47

---
+++ pms/pms/doctype/pms_maintenance_request/pms_maintenance_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Lloyd Lariosa and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("PMS Maintenance Request", {
+// 	refresh(frm) {
+
+// 	},
+// });
