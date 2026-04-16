33b9c13709836bdd24fba7a2ab6616a263a7ce65
loydfordfarrow <loydfordfarrow@gmail.com>
2026-04-16 08:16:15 +0800

    2026-04-16 08:16:15

--- pms/pms/doctype/pms_contract/pms_contract.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Lloyd Lariosa and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("PMS Contract", {
-// 	refresh(frm) {
-
-// 	},
-// });
